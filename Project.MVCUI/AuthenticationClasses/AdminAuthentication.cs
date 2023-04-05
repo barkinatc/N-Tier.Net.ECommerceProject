85f35d8eaa4c1bbf184d96d07ac2ff7b7b15d4f5
Barkın Atıcı <brknatici@hotmail.com>
2023-04-05 23:50:26 +0300

    Sepet oluşturuldu

--- Project.MVCUI/AuthenticationClasses/AdminAuthentication.cs
+++ Project.MVCUI/AuthenticationClasses/AdminAuthentication.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Project.MVCUI.AuthenticationClasses
 {
-    public class AdminAuthentication
+    public class AdminAuthentication:AuthorizeAttribute
     {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Session["admin"]!=null)
+            {
+                return true;
+            }
+            httpContext.Response.Redirect("/Home/Login");
+            return false;
+        }
     }
 }
