19003f13449d9e98ec195afa334c10ffc816fbc5
Barkın Atıcı <brknatici@hotmail.com>
2023-04-07 19:17:33 +0300

    Kategori ekleme

--- Project.MVCUI/Areas/Admin/AdminAreaRegistration.cs
+++ Project.MVCUI/Areas/Admin/AdminAreaRegistration.cs
@@ -15,10 +15,10 @@ namespace Project.MVCUI.Areas.Admin
         public override void RegisterArea(AreaRegistrationContext context) 
         {
             context.MapRoute(
-                "Admin_default",
-                "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
-            );
+                    "Admin_default",
+                    "Admin/{controller}/{action}/{id}",
+                    new { action = "Index", id = UrlParameter.Optional }
+                );
         }
     }
 }
--- Project.MVCUI/Areas/Admin/Controllers/CategoryController.cs
+++ Project.MVCUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Project.BLL.Repositories.ConcRep;
+using Project.ENTITIES.Models;
+using Project.MVCUI.Areas.Admin.Data.AdminPageVMs;
+using Project.VM.PureVMs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +12,65 @@ namespace Project.MVCUI.Areas.Admin.Controllers
 {
     public class CategoryController : Controller
     {
-        // GET: Admin/Category
-        public ActionResult ListCategories()
+        CategoryRepository _cRep;
+
+        public CategoryController()
+        {
+            _cRep = new CategoryRepository();
+        }
+        public ActionResult ListCategories(int? id)
+        {
+            List<AdminCategoryVM> categories;
+            if (id==null)
+            {
+                categories = _cRep.Select(x => new AdminCategoryVM
+                {
+                    ID = x.ID,
+                    CategoryName = x.CategoryName,
+                    Description = x.Description,
+                    CreatedDate = x.CreatedDate,
+                    ModifiedDate = x.ModifiedDate,
+                    DeletedDate = x.DeletedDate,
+                    Status = x.Status.ToString()
+                }).ToList();
+            }
+            else
+            {
+                categories = _cRep.Where(x => x.ID == id).Select(x => new AdminCategoryVM
+                {
+                    ID = x.ID,
+                    CategoryName = x.CategoryName,
+                    Description = x.Description,
+                    CreatedDate = x.CreatedDate,
+                    ModifiedDate = x.ModifiedDate,
+                    DeletedDate = x.DeletedDate,
+                    Status = x.Status.ToString()
+                }).ToList();
+            }
+            AdminCategoryListPageVM aclpvm = new AdminCategoryListPageVM
+            {
+                Categories = categories
+            };
+            return View(aclpvm);
+        }
+
+        public ActionResult AddCategory() 
         {
             return View();
+        }   
+
+        [HttpPost]
+        public ActionResult AddCategory(AdminCategoryVM category)
+        {
+            Category c = new Category
+            {
+                CategoryName = category.CategoryName,
+                Description = category.Description
+            };
+ 
+            _cRep.Add(c);
+
+            return RedirectToAction("ListCategories");
         }
     }
 }
